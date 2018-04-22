4e63674ef936ffcc1fd54a921839cf0dd2f8369a
Fayez Altamimi <fox880919@gmail.com>
2018-04-23 01:47:42 +0300

    Before Improving Design

---
+++ Yoga Manager/Classes/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  Yoga Manager
+//
+//  Created by Fayez Altamimi on 23/04/2018.
+//  Copyright © 2018 Fayez Altamimi. All rights reserved.
+//
+
+import Foundation
+
+class Colors{
+    
+    let exPrimaryColor = UIColor(red:0.07, green:0.18, blue:0.24, alpha:1.0)
+
+    let exPecondaryColor = UIColor(red:0.40, green:0.88, blue:0.97, alpha:1.0)
+
+    
+    let PrimaryColor = UIColor(red:0.07, green:0.18, blue:0.24, alpha:1.0)
+    
+    let secondaryColor = UIColor(red:0.40, green:0.88, blue:0.97, alpha:1.0)
+}
